71e91a9cf59826a222ef321b0defca4a46dd9ec4
Fikri Prasetya <fikriprasetya3@gmail.com>
2024-06-19 04:16:23 +0700

    update discover button

--- src/components/DiscoverButton/index.jsx
+++ src/components/DiscoverButton/index.jsx
@@ -4,12 +4,12 @@ const DiscoverButton = (props) => {
   return (
     <div>
       <button className="overflow-hidden w-auto p-2 h-12 bg-black text-white border-none rounded-md text-xl font-bold cursor-pointer relative z-10 group" onClick={props.onClick}>
-        Discover More!
+        Login
         <span className="absolute w-full ml-1 h-36 -top-8 -left-1 bg-white rotate-10 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-left"></span>
         <span className="absolute w-full ml-1 h-36 -top-8 -left-1 bg-[#F4E6D7] rotate-10 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-left"></span>
         <span className="absolute w-full ml-1 h-36 -top-8 -left-1 bg-[#EDDFD1] rotate-10 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-left"></span>
         <span className="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-11 z-10 text-black">
-          Explore!
+          Admin Only!
         </span>
       </button>
     </div>
