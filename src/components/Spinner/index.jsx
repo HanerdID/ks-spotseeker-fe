af1b32eb928a8f802e339b3eb62b81b3aaef5955
Fikri Prasetya <fikriprasetya3@gmail.com>
2024-06-19 18:42:12 +0700

    update loader

--- src/components/Spinner/index.jsx
+++ src/components/Spinner/index.jsx
@@ -6,9 +6,8 @@ const Spinner = () => {
     <div>
       <div className="flex w-dvw h-dvh justify-center items-center fixed opacity-50 bg-black z-50 top-0">
         <div className="loader">
-          <div data-glitch="Loading..." className="glitch">
-            Loading...
-          </div>
+          <span className="loader-text">loading</span>
+          <span className="load"></span>
         </div>
       </div>
     </div>
